609768ab6d5514c3a1b938e7e7ff0b499316fd9e
muhammaddawoodsaeed <dawoodsaeed.work@gmail.com>
2025-03-19 09:32:20 +0500

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-purple-500 h-full">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
